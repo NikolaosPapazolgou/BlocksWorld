2f2a918039db2c6597dce680df8e6af920c3c5e3
NikolaosPapazolgou <nikospapazoglou9orfeas@gmail.com>
2024-11-21 16:15:17 +0200

    Creates a dictionary with 5 key pair values the first is the blocks list, the second is the blocks list which have the clear property set to true, the third one is the ontable blocks list, the 4th one is the on list of blocks , the 5th one is the goal set of tuples.Also processes this data and creates the initial state of the enviroment the TABLE list of stacks of blocks

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import re
+
 #NOTE: POSSIBLE POSITIONS OF EACH BLOCK
 # We have a table and a number of blocks.A block can be ontop of the table (ontable A)
 # OR it can be ontop of another block (on A B).
@@ -23,33 +25,73 @@
 # Read the input file and Write the solution to the output file
 #TODO GENERAL
 #TODO Read the INPUT.TXT File
-alphabet = [chr(i) for i in range(65, 91)]
-print(alphabet)
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 with open('input.txt', "r") as inputFile:
-    line_2 = inputFile.read()
     lines = inputFile.readlines()
 
-
-print(line_2.strip()[1:-1].split(') ('), "DSASDDSAASD")
+#The lines of the file from 2 to 5
 useful_lines = lines[2:5]
 print(useful_lines)
 
-chunk_size = 10
-block_properties_list = []
+# Processing the file input,while creating a dictionary that includes
+# 3 keys : 1) blocks 2) clear 3) ontable 4) on 5) goal
+#The 1. Indicates how many blocks are being used and what's their name
+#The 2. Indicates how many of them will have a property of clear true
+#The 3. Indicates how many of the blocks will have a property of ontable true
+#The 4. Indicates which blocks are on top of each other for instance: on A B means that A is on top of B
+#The 5. Indicates which is the end state goal, it contains on pairs
+dictionary_formatted_input = {}
 
 for line in useful_lines:
     if "objects" in line:
-        for character in line:
-            if character in alphabet:
-                print(character)
-
+        current_list = [char for char in line if char in alphabet]
+        dictionary_formatted_input["blocks"] = current_list
     if "INIT" in line:
-        for line_chunk in range(7, len(line), chunk_size):
-            if "ONTABLE" in line[line_chunk: line_chunk + chunk_size]:
-                chunk_size = 12
-            print(line[line_chunk: line_chunk + chunk_size])
+        clear_letters = re.findall(r'\(CLEAR (\w)\)', line)
+        ontable_letters = re.findall(r'\(ONTABLE (\w)\)', line)
+        on_letters = re.findall(r'\(ON (\w) (\w)\)', line)
+        dictionary_formatted_input["clear"] = clear_letters
+        dictionary_formatted_input["ontable"] = ontable_letters
+        dictionary_formatted_input["on"] = on_letters
+    if "goal" in line:
+        on_letters = re.findall(r'\(ON (\w) (\w)\)', line)
+        dictionary_formatted_input["goal"] = on_letters
 
+for block in dictionary_formatted_input["blocks"]:
+    print("BLOCKS :", block)
+for clear_block in dictionary_formatted_input["clear"]:
+    print("CLEAR BLOCKS:", clear_block)
+for ontable_block in dictionary_formatted_input["ontable"]:
+    print("ONTABLE BLOCKS:", ontable_block)
+for on_block in dictionary_formatted_input["on"]:
+    print("ON BLOCKS:", on_block, type(on_block))
+for goal_block in dictionary_formatted_input["goal"]:
+    print("GOAL BLOCKS:", goal_block, type(goal_block))
+
+#TODO: INITIAL STATE CONTAINS THE ENVIROMENT IN THIS OCCASION IS THE TABLE WHICH IS A SET OF STACKS
+#I will create a list of lists to represent the set of stacks (TABLE)
+table = []
+
+
+def create_initial_state(table, dictionary_formatted_input):
+    for ontable_block in dictionary_formatted_input["ontable"]:
+        list = [ontable_block]
+        table.append(list)
+    for on_block in reversed(dictionary_formatted_input["on"]):
+        first_letter = on_block[0]
+        second_letter = on_block[1]
+        print(f"FIRST LETTER: {first_letter}", f"SECOND LETTER: {second_letter}")
+        for stack in table:
+            print(f"STACK : {stack}")
+            if second_letter in stack:
+                print("INSIDE THE IF THE STACK")
+                stack.append(first_letter)
+
+
+create_initial_state(table, dictionary_formatted_input)
+
+print(f"TABLE {table}")
 
 #TODO Process The INPUT.TXT File content
 #TODO Store The Initial state
